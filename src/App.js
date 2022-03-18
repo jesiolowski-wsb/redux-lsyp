d2c53d74d40a8a518968fb8ec90fcb5c73aca60e
Filip Jesiolowski <filip.jesiolowski.sp@lhsystems.com>
2022-03-18 10:45:04 +0100

    mapStateToProps added + connected App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+  console.log(props);
+  return <div className="App"></div>;
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts,
+  };
+};
+
+export default connect(mapStateToProps)(App);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,4 +1,11 @@
-const initState = { posts: [] };
+const initState = {
+  posts: [
+    { id: 1, title: "title 1", body: "Lorem ipsum..." },
+    { id: 2, title: "title 2", body: "Lorem ipsum..." },
+    { id: 3, title: "title 3", body: "Lorem ipsum..." },
+    { id: 4, title: "title 4", body: "Lorem ipsum..." },
+  ],
+};
 
 const rootReducer = (state = initState, action) => {
   return state;
